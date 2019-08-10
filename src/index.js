e813b7199e422cc98bc95b32f7f6a7f331a23e0d
Mónica Sánchez Santos <monni@MacBook-Pro-de-Yoshua-2.local>
2019-08-09 23:21:05 -0500

    Add Header

--- src/index.js
+++ src/index.js
@@ -2,9 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Header from './components/Header'
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+
 
 ReactDOM.render(
-    <App />, 
+    <Router>
+        <Header>
+        </Header>
+    </Router>, 
     document.getElementById('root')
 );
+
 

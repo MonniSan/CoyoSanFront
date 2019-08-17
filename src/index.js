ff7fcc0d7c7cc3d5206f221356a48c059276eba6
Mónica Sánchez Santos <monni@MacBook-Pro-de-Yoshua-2.local>
2019-08-16 21:28:37 -0500

    creando componentes de vistas.

--- src/index.js
+++ src/index.js
@@ -2,14 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
-import Header from './components/Header'
+import Header from './components/Header';
+import Home from './views/home';
+import Login from './views/login';
+import Signup from './views/signup';
+import Summary from './views/summary';
+import Settings from './views/settings';
+import Password from './views/password';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 
 
 ReactDOM.render(
     <Router>
-        <Header>
-        </Header>
+        <Header/>
+        <Switch>
+            <Route exact path="/" component={Home}/>
+            <Route exact path="/login" component={Login} />
+            <Route exact path="/signup" component={Signup} />
+            <Route exact path="/summary" component={Summary} />
+            <Route exact path="/settings" component={Settings} />
+            <Route exact path="/password" component={Password} />
+            <Route>
+                <h1>Error 404</h1>
+            </Route>
+        </Switch>
     </Router>, 
     document.getElementById('root')
 );
---
+++ src/views/password.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Password = props => (
+    <div>
+        <h1>Recupera tu contraseña</h1>
+    </div>
+)
+
+export default Password

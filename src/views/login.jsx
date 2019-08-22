68751eac0592e03fb3e0f7b42d7d9a13d37f0505
leureva <leureva@gmail.com>
2019-08-22 17:44:54 -0500

    update vista login

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,16 +1,42 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import '../components/Login.css'
+
 const Login = props => (
-    <div>
-        <label>E-mail</label>
-        <input type="email" ></input>
-        <label>Contraseña</label>
-        <input type="password" ></input>
-        <NavLink to="/summary">
-            <button>Entrar</button>
-        </NavLink>
-        <p>Olvidaste tu contraseña haz click <NavLink to="/password">aqui</NavLink></p>
+    <div style={{ backgroundColor: "#E5E9EA" }}>
+        <div class="container" style={{ paddingTop: "30px", paddingBottom: "30px" }}>
+            <div class="row justify-content-center">
+                <div class="col-md-9">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form>
+                                        <div class="form-group row">
+                                            <label for="email" class="col-4 col-form-label">Email</label>
+                                            <div class="col-8">
+                                                <input id="email" name="email" class="form-control here" required="required" type="text" />
+                                            </div>
+                                        </div>
+                                        <div class="form-group row">
+                                            <label for="email" class="col-4 col-form-label">Contraseña</label>
+                                            <div class="col-8">
+                                                <input id="password" name="password" class="form-control here" required="required" type="text" />
+                                                <p style={{ paddingTop: "5px" }}>Si olvidaste tu contraseña, haz clic <a class="anchor" href="">aquí</a></p>
+                                            </div>
+                                        </div>
+                                        <div class="form-group row justify-content-center">
+                                            <button name="submit" type="submit" class="btn">Entrar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+
 )
 
 export default Login
--- src/views/settings.jsx
+++ src/views/settings.jsx
@@ -5,7 +5,7 @@ const Settings = props => (
 
     <div style={{ backgroundColor: "#E5E9EA" }}>
         <div class="container" style={{ paddingTop: "30px", paddingBottom: "30px" }}>
-            <div class="row" style={{ marginBottom: "15px" }}>
+            <div class="row">
                 <div class="col-md-3">
                     <div class="list-group ">
                         <a href="#" class="list-group-item list-group-item-action active settings-menu">Perfil</a>
@@ -28,32 +28,32 @@ const Settings = props => (
                                         <div class="form-group row">
                                             <label for="username" class="col-4 col-form-label">Nombre de usuario</label>
                                             <div class="col-8">
-                                                <input id="username" name="username" placeholder="Nombre de usuario" class="form-control here" required="required" type="text" />
+                                                <input id="username" name="username" class="form-control here" required="required" type="text" />
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label for="name" class="col-4 col-form-label">Nombre</label>
                                             <div class="col-8">
-                                                <input id="name" name="name" placeholder="Nombre" class="form-control here" required="required" type="text" />
+                                                <input id="name" name="name" class="form-control here" required="required" type="text" />
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label for="lastname" class="col-4 col-form-label">Apellidos</label>
                                             <div class="col-8">
-                                                <input id="lastname" name="lastname" placeholder="Apellidos" class="form-control here" required="required" type="text" />
+                                                <input id="lastname" name="lastname" class="form-control here" required="required" type="text" />
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label for="email" class="col-4 col-form-label">Email</label>
                                             <div class="col-8">
-                                                <input id="email" name="email" placeholder="Email" class="form-control here" required="required" type="text" />
+                                                <input id="email" name="email" class="form-control here" required="required" type="text" />
                                             </div>
                                         </div>
                                         <div class="form-group row">
                                             <label for="email" class="col-4 col-form-label">Contraseña</label>
                                             <div class="col-8">
-                                                <input id="password" name="password" placeholder="Contraseña" class="form-control here" required="required" type="text" />
-                                                <a class="anchor" href="/password">Reestablecer contraseña</a>
+                                                <input id="password" name="password" class="form-control here" required="required" type="text" />
+                                                <p style={{ marginTop: "5px" }}><a class="anchor" href="/password">Reestablecer contraseña</a></p>
                                             </div>
                                         </div>
                                         <div class="form-group row">

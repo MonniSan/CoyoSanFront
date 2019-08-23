a4982b5b5fc89c1db7df56808989b948cef89a75
Mónica Sánchez Santos <monni@MacBook-Pro-de-Yoshua-2.local>
2019-08-22 20:22:54 -0500

    add view

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,40 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom'
-import './Header.css'
-import Logo from '../assets/images/logoCoyoSan.jpg'
+import React from "react";
+import { NavLink } from "react-router-dom";
+import "./Header.css";
+import Logo from "../assets/images/logoCoyoSan.jpg";
 
-const Header = () =>
-    <div class="nav-header">
-        <div class="container">
-            <div class="row align-items-center">
-                <div class="col-md-3 col-sm-12">
-                    <div class="nav-logo">
-                        <NavLink exact to="/"><img src={Logo} alt="CoyoSan logo" /></NavLink>
-                    </div>
-                </div>
-                <div class="col-md-9 d-none d-md-block">
-                    <div class="main-menu">
-                        <nav class="nav-menu">
-                            <ul id="nav_menu">
-                                <li><NavLink data-hover="¡Coyosan espera!" to="/login"><span>Entrar</span></NavLink></li>
-                                <li><NavLink data-hover="¡Vamos!" to="/signup"><span>Registrarse</span></NavLink></li>
-                            </ul>
-                        </nav>
-                    </div>
-                </div>
-            </div>
+const Header = () => (
+  <div class="nav-header">
+    <div class="container">
+      <div class="row align-items-center">
+        <div class="col-md-3 col-sm-12">
+          <div class="nav-logo">
+            <NavLink exact to="/">
+              <img src={Logo} alt="CoyoSan logo" />
+            </NavLink>
+          </div>
         </div>
+        <div class="col-md-9 d-none d-md-block">
+          <div class="main-menu">
+            <nav class="nav-menu">
+              <ul id="nav_menu">
+                <li>
+                  <NavLink data-hover="¡Coyosan espera!" to="/login">
+                    <span>Entrar</span>
+                  </NavLink>
+                </li>
+                <li>
+                  <NavLink data-hover="¡Vamos!" to="/signup">
+                    <span>Registrarse</span>
+                  </NavLink>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </div>
+      </div>
     </div>
-    
-    
+  </div>
+);
 
-
-export default Header
+export default Header;
---
+++ src/views/startgame.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Start = props => (
+  <div>
+    <h1>Registro exitoso</h1>
+    <h5> Hola ! Yo soy tu sensei //name.sensei// muchas gracias por crearme</h5>
+    <div>
+      <p>
+        Tu meta //name.goal// es muy interesante, y se que juntos lograremos
+        hacerla
+        <br />
+        realidad. Para logar tu meta deberas ahorrar $//amountToBe//
+        mensualmente y
+        <br />
+        puedes empezar HOY!
+      </p>
+    </div>
+    <div>
+      <p>
+        Realiza tu primer deposito para empezar la aventura.
+        <button> Deposito realizado </button>
+      </p>
+    </div>
+  </div>
+);
+
+export default Start;

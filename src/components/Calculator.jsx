3f7747813708e44660a67ac37a530262617f8246
Mónica Sánchez Santos <monni@MacBook-Pro-de-Yoshua-2.local>
2019-08-27 01:28:51 -0500

    Calculator update

--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,65 +1,140 @@
-import React from 'react'
-import './Calculator.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Calculator.css";
+import { Formik } from "formik";
 
 // stateless component
-const Calculator = ({ handleChange, goal_name, goal_price, goal_months, submit, errors }) => (
-    <div className="d-flex justify-content-center">
-        <div className="home-calculator">
-
-            <div className="form-row">
-                <div className="form group-row col">
-                    <label htmlFor="inputState">¿Cuál es tu sueño?</label>
-                    <select type="select" id="inputState" className={`form-control ${errors['goal_name'] ? 'is-invalid' : ''}`} onChange={handleChange('goal_name')} value={goal_name}>
-                        <option>¡Elige!</option>
-                        <option value="house">Casa</option>
-                        <option value="education">Educación</option>
-                        <option value="wedding">Retiro</option>
-                        <option value="holydays">Vacaciones</option>
-                        <option value="other">Otro</option>
-                    </select>
-                    {
-                        errors['goal_name'] ?
-                            (<div class="invalid-feedback">{errors['goal_name']}</div>) : null
-                    }
-                </div>
-            </div>
-
-            <div className="form-row">
-                <div className="form group-row col">
-                    <label htmlFor="inputState">¿Cuánto cuesta?</label>
-                    <div className="input-group">
-                        <div className="input-group-prepend">
-                            <span className="input-group-text">$</span>
-                        </div>
-                        <input type="number" className={`form-control ${errors['goal_price'] ? 'is-invalid' : ''}`} onChange={handleChange('goal_price')} value={goal_price} />
-                        {
-                            errors['goal_price'] ?
-                                (<div class="invalid-feedback">{errors['goal_price']}</div>) : null
-                        }
-                    </div>
-                </div>
-            </div>
-
-            <div className="form-row">
-                <div className="form group-row col">
-                    <label htmlFor="inputState">¿En cuántos meses quieres lograrlo?</label>
-                    <input type="number" className={`form-control ${errors['goal_months'] ? 'is-invalid' : ''}`} onChange={handleChange('goal_months')} value={goal_months} />
-                    {
-                        errors['goal_months'] ?
-                            (<div class="invalid-feedback">{errors['goal_months']}</div>) : null
-                    }
-                    {goal_months > 0 ? (<span className="help-text">{(goal_months / 12).toFixed(1)} años</span>) : null}
-
-                </div>
-            </div>
-
-            <div className="mt-3">
-                <button type="button" class="btn btn-outline-success btn-block" onClick={submit}>
-                    Calcular
-                </button>
-            </div>
+const Calculator = ({
+  validationSchema,
+  handleChange,
+  goal_name,
+  goal_price,
+  goal_months,
+  submit,
+  errors,
+  goal,
+  message,
+  handleForm
+}) => (
+  <div className="d-flex justify-content-center">
+    <div className="home-calculator">
+      {!message && (
+        <h5>
+          Elige tu primer objetivo de ahorro con la práctica calculadora
+          financiera que CoyoSan preparó para ti.
+        </h5>
+      )}
+      {message && (
+        <div>
+          <h5>{message}</h5>
+          <Link class="btn btn-outline-success btn-block" to="/signup">
+            Registrarse
+          </Link>
         </div>
+      )}
+      <Formik
+        validationSchema={validationSchema}
+        initialValues={{
+          goal_name: "",
+          goal_price: 0,
+          goal_months: 6
+        }}
+        onSubmit={submit}
+      >
+        {({
+          values,
+          errors,
+          touched,
+          handleBlur,
+          handleChange,
+          handleSubmit,
+          isSubmitting
+        }) => (
+          <form onSubmit={handleSubmit}>
+            {!isSubmitting && (
+              <>
+                <div className="form-row">
+                  <div className="form group-row col">
+                    <label>¿Cuál es tu sueño?</label>
+                    <select
+                      type="text"
+                      placeholder="Sueño"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="goal_name"
+                      value={values.goal_name}
+                      class="form-control"
+                    >
+                      <option>¡Elige!</option>
+                      <option value="Casa">Casa</option>
+                      <option value="Educación">Educación</option>
+                      <option value="Retiro">Retiro</option>
+                      <option value="Vacaciones">Vacaciones</option>
+                      <option value="Otro">Otro</option>
+                    </select>
+                    {errors.goal_name && touched.goal_name && (
+                      <div>
+                        {errors.goal_name} <br />
+                        <br />
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="form group-row col">
+                    <label>¿Cuánto cuesta?</label>
+                    <input
+                      class="form-control"
+                      type="number"
+                      placeholder="Costo"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="goal_price"
+                      value={values.goal_price}
+                    />
+                    {errors.goal_price && touched.goal_price && (
+                      <div>
+                        {errors.goal_price} <br />
+                        <br />
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="form-row">
+                  <div className="form group-row col">
+                    <label>¿En cuantos meses quieres lograrlo?</label>
+                    <input
+                      class="form-control"
+                      type="number"
+                      placeholder="Meses"
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      name="goal_months"
+                      value={values.goal_months}
+                    />
+                    {errors.goal_months && touched.goal_months && (
+                      <div>
+                        {errors.goal_months} <br />
+                        <br />
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <div className="mt-3">
+                  <button
+                    type="submit"
+                    className="btn btn-outline-success btn-block"
+                  >
+                    Calcular
+                  </button>
+                </div>
+              </>
+            )}
+          </form>
+        )}
+      </Formik>
     </div>
+  </div>
 );
 
-export default Calculator
+export default Calculator;
--- src/containers/Calculator.jsx
+++ src/containers/Calculator.jsx
@@ -1,63 +1,67 @@
-import React from 'react';
-import Calculator from '../components/Calculator';
-import Loading from '../components/Loading';
+import React from "react";
+import Calculator from "../components/Calculator";
+import Loading from "../components/Loading";
+import * as Yup from "yup";
 
 class CalculatorContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isLoading: false,
+      message: ""
+    };
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            goal_name: '',
-            goal_price: 0,
-            goal_months: 0,
-            isLoading: false,
-            errors: {}
-        }
-    }
+    this.handleChange = this.handleChange.bind(this);
+    this.submit = this.submit.bind(this);
+  }
 
-    handleChange(property) {
-        return (event) => {
-            this.setState({ [property]: event.target.value });
-        }
-    }
+  handleChange(property) {
+    return event => {
+      this.setState({ [property]: event.target.value });
+    };
+  }
 
-    submit() {
-        const { goal_name, goal_price, goal_months } = this.state;
+  submit(values) {
+    const { goal_price, goal_months } = values;
+    // Caso todo chido, voy al servidor
+    const precision = Math.pow(10, 2);
+    const regularAmountToBe = goal_price / goal_months;
+    const regularAmountToBeRounded =
+      Math.ceil(regularAmountToBe * precision) / precision;
+    this.setState({
+      message: `Necesitas ahorrar durante ${goal_months} meses la cantidad de $${regularAmountToBeRounded}`
+    });
+  }
 
-        let errors = {};
+  render() {
+    const { handleChange, submit, handleForm } = this;
+    const { isLoading } = this.state;
+    const validationSchema = Yup.object().shape({
+      goal_name: Yup.string("Se necesita una meta")
+        .min(3, "Se necesita una meta")
+        .required("Se necesita una meta"),
+      goal_months: Yup.number("Dato inválido")
+        .positive("Dato inválido")
+        .min(6, "Se necesita más de 6 meses")
+        .required("Se necesita un plazo de tiempo"),
+      goal_price: Yup.number("Dato inválido")
+        .positive("Dato inválido")
+        .min(1, "Se necesita una cantidad más grande")
+        .required("Se necesita escribir el precio")
+    });
 
-        if (goal_price <= 0) {
-            errors['goal_price'] = 'Proporciona una cantidad o monto válido.';
-        }
-
-        if (goal_months < 6) {
-            errors['goal_months'] = 'Elige una meta a mediano o largo plazo.';
-        }
-
-        if (!goal_name) {
-            errors['goal_name'] = 'El nombre es obligatorio.';
-        }
-
-        if (Object.keys(errors).length) {
-            this.setState({ errors });
-        } else {
-            // Caso todo chido, voy al servidor
-            this.setState({ isLoading: true });
-
-            
-        }
-    }
-
-    render() {
-        const { handleChange, submit } = this;
-        const { isLoading } = this.state;
-
-        return (
-            isLoading ?
-                <Loading /> :
-                <Calculator {...this.state} handleChange={handleChange.bind(this)} submit={submit.bind(this)} />
-        );
-    }
+    return isLoading ? (
+      <Loading />
+    ) : (
+      <Calculator
+        {...this.state}
+        handleChange={handleChange}
+        submit={submit}
+        handleForm={handleForm}
+        validationSchema={validationSchema}
+      />
+    );
+  }
 }
 
 export default CalculatorContainer;
